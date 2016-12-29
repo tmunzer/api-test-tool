437fa6364110a691aecfe7004cdd4da035c8a30c
Thomas Munzer <thomas@mbp-tmunzer.local>
2016-12-29 16:31:38 +0100

    added  "cloud-va2.aerohive.com"

--- routes/login.js
+++ routes/login.js
@@ -25,7 +25,7 @@ router.get('/', function (req, res, next) {
 router.post('/', function (req, res, next) {
     var ownerIdRegexp = new RegExp("^[0-9]*$");
     var accessTokenRegexp = new RegExp("^[^ ]{40}$");
-    var apiServers = ["cloud-va.aerohive.com", "cloud-ie.aerohive.com"];
+    var apiServers = ["cloud-va.aerohive.com", "cloud-va2.aerohive.com", "cloud-ie.aerohive.com"];
     if (!(req.body.vpcUrl && apiServers.indexOf(req.body["vpcUrl"]) >= 0)) {
         res.redirect("/?errorcode=1");
     } else if (!(req.body.ownerId && ownerIdRegexp.test(req.body['ownerId']))) {
@@ -46,7 +46,7 @@ router.post('/', function (req, res, next) {
 router.post('/op', function (req, res, next) {
     var ownerIdRegexp = new RegExp("^[0-9]*$");
     var accessTokenRegexp = new RegExp("^[a-zA-Z0-9]{40}$");
-    var apiServers = ["cloud-va.aerohive.com", "cloud-ie.aerohive.com"];
+    var apiServers = ["cloud-va.aerohive.com", "cloud-va2.aerohive.com", "cloud-ie.aerohive.com"];
     if (!(req.body.vpcUrl && req.body["vpcUrl"] != "")) res.redirect("/?errorcode=1");
     else if (!(req.body.ownerId) && ownerIdRegexp.test(req.body['ownerId'])) res.redirect("/?errorcode=2");
     else if (!(req.body.accessToken && accessTokenRegexp.test(req.body["accessToken"].trim()))) res.redirect("/?errorcode=3");
