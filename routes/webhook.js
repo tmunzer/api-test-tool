565230fa6a039b751ae44e34cbcbfe79340f516f
Thomas Munzer <thomas@mbp-tmunzer.local>
2016-12-31 17:38:40 +0100

    enabled socket.io for webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,15 +1,13 @@
 
 var express = require('express');
 var router = express.Router();
-//var io = require('socket.io')(server);
-
+var io = require('socket.io')(server);
 
 
 /* GET users listing. */
-router.post('/presence', function (req, res, next) {
-    //if (req.session.webhookId == )
+router.post('/presence', function (req, res, next) {    
     console.log(req);
-    //io.sockets.in(req.session.webhookId).emit("webhook data", req.body)
+    io.sockets.in(req.session.webhookId).emit("webhook data", req.body)
     res.send();
 });
 
