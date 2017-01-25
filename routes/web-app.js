99fd18cfff9f98f430c4d4d5fc0ca0be6dcef91f
Thomas Munzer <thomas@mbp-tmunzer.local>
2017-01-24 22:04:53 -0800

    test

--- routes/web-app.js
+++ routes/web-app.js
@@ -15,8 +15,11 @@ function createSocket(req) {
             socket.on('update', function (action) {
                 socket.emit("update", action); // send the update message to the sender
                 socket.broadcast.emit("update", action); // send the update message to everyone in this nsp BUT the sender
-            });
+            });            
         });
+        nsp.on('disconnect', function(socket){
+            console.log("disconnected");
+        })
     }
 }
 /*================================================================
