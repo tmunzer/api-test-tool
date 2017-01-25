d7e6c043c0fdff68815f4c36dcb862597d95bcb9
Thomas Munzer <thomas@mbp-tmunzer.local>
2017-01-25 11:35:32 -0800

    fix nodejs v6 comp

--- routes/web-app.js
+++ routes/web-app.js
@@ -51,7 +51,7 @@ function createSocket(req) {
                 setTimeout(function () {
                     var count = 0;
                     for (var prop in socket.nsp.connected) {
-                        if (obj.hasOwnProperty(prop))
+                        if (obj.prop)
                             ++count;
                     }
                     if (count == 0) {
